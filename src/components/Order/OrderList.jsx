69844ac485059f1a5700b20c71059eb02c45ab27
Sayed-Udoy <umudoy364@gmail.com>
2025-03-26 06:55:06 -0700

    all is done

--- src/components/Order/OrderList.jsx
+++ src/components/Order/OrderList.jsx
@@ -5,7 +5,7 @@ import { CiEdit } from "react-icons/ci";
 
 const OrderListHeader = () => {
   return (
-    <div className=" bg-primary2/30 shadow-md shadow-white/5 flex flex-nowrap justify-between px-2 py-2 font-semibold text-center text-grayText">
+    <div className=" bg-primary2/30 text-start min-w-[1000px] shadow-md shadow-white/5 flex flex-nowrap justify-between px-2 py-2 font-semibold text-grayText">
       <div className="flex flex-1 gap-2 items-center justify-center">
         <input className="w-3 h-3" type="checkbox" />
         <p>order</p>
@@ -22,15 +22,15 @@ const OrderListHeader = () => {
 
 const OrderList = () => {
   return (
-    <section className="text-white bg-primary2/10 overflow-x-auto cursor-pointer hide-scrollbar w-[100p0x]">
+    <section className="text-white bg-primary2/10 overflow-x-auto cursor-pointer hide-scrollbar ">
       <div className="min-w-[700px]">
         <OrderListHeader />
       </div>
-      <div className="mt-5 space-y-2 text-white/70 min-w-[700px] font-medium text-[14px]">
+      <div className="mt-5 space-y-2 text-white/70 min-w-[1000px] font-medium text-[14px]">
         {ordersList.map((od, idx) => (
           <div
             key={od.id}
-            className="bg-primary1 flex items-center flex-nowrap gap-5 border-t border-b border-grayText/10 py-2 justify-between px-2 text-center"
+            className="bg-primary1 text-start flex items-center flex-nowrap gap-5 border-t border-b border-grayText/10 py-2 justify-between px-2"
           >
             <div className="flex-1 flex items-center gap-2 justify-center">
               <input className="w-3 h-3" type="checkbox" />
@@ -51,7 +51,7 @@ const OrderList = () => {
             </div>
             <div className="flex-1">{od.total}</div>
             <div className="flex-1">N/A</div>
-            <div className="flex-1 text-xl flex justify-center gap-5">
+            <div className="flex-1 text-xl  flex items-center mr-4 justify-start md:gap-2 lg:gap-5">
               <button className="cursor-pointer">
                 <MdDeleteOutline className="text-red-500/70" />
               </button>
